df21205855cbaeae5f6abb3202540c5d51eae828
Dušan Rychnovský <dusan.rychnovsky@gmail.com>
2025-06-23 09:41:11 +0200

    Cargo init

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod data_file_parser;
+
+fn main() {
+  data_file_parser::parse();
+}
---
+++ tests/data_file_parser_tests.rs
@@ -0,0 +1,4 @@
+#[test]
+fn parse_given_empty_file_returns_empty_tree() {
+  panic!("Implement the parse function in data_file_parser.rs");
+}
